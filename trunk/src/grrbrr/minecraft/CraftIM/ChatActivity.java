25642ad33c0df68e74dfc988e5f6c1e3536279d4
nicolai.schoenberg@gmail.com <nicolai.schoenberg@gmail.com@2ee48787-b125-5634-494c-b4c01e679e7e>
2011-02-18 08:27:39 +0000

    Initial Checkin

---
+++ trunk/src/grrbrr/minecraft/CraftIM/ChatActivity.java
@@ -0,0 +1,12 @@
+package grrbrr.minecraft.CraftIM;
+
+import android.app.Activity;
+import android.os.Bundle;
+import grrbrr.minecraft.CraftIM.R;
+
+public class ChatActivity extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat);
+    }
+}
